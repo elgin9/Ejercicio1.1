f8fb9e96b3c5252616885f63b701805d6457bb0c
ElginPC <esmaldonado@uth,hn>
2024-09-29 00:18:36 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1.1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio1._1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
